896e68941ba46bf12beaf58cfa0152f96ce7de2a
Mukesh <67510024+Mukesh183@users.noreply.github.com>
2023-09-09 20:02:18 +0530

    Update BasicTables2Examples.java

--- LearnSelenium/src/main/java/learnselenium/BasicTables2Examples.java
+++ LearnSelenium/src/main/java/learnselenium/BasicTables2Examples.java
@@ -3,7 +3,6 @@ package learnselenium;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
